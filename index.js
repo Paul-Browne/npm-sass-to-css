9f80939d59d37ef1818ca853b8da010f81269846
Paul Browne <paul.browne@newthings.co>
2021-10-21 11:37:12 +0300

    async

--- index.js
+++ index.js
@@ -1,68 +1,67 @@
-const sass = require('sass');
-const less = require('less');
-const postcss = require('postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const mime = require('mime-types');
-const path = require('path');
-const fs = require('fs');
+import sass from 'sass';
+const {render:renderSass} = sass;
+import less from 'less';
+const {render:renderLess} = less;
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import { lookup } from 'mime-types';
+import { dirname } from 'path';
+import { mkdir, writeFile, readFile } from 'fs/promises';
 
-const type = inPath => {
-	if(mime.lookup(inPath) === 'text/css'){
+const type = path => {
+	if(lookup(path) === 'text/css'){
 		return 'css'
-	}else if (mime.lookup(inPath) === 'text/less') {
+	}else if (lookup(path) === 'text/less') {
 		return 'less'
-	}else if (mime.lookup(inPath) === 'text/x-scss' || mime.lookup(inPath) === 'text/x-sass') {
+	}else if (lookup(path) === 'text/x-scss' || lookup(path) === 'text/x-sass') {
 		return 'sass'
 	}
 }
 
-const makeFileSync = (_path, contents) => {
-	if(contents){
-		fs.mkdir(path.dirname(_path), { recursive: true }, (err) => {
-			if (err) throw err;
-			fs.writeFileSync(_path, contents);
-		});
+const makeFile = async (path, contents) => {
+	if (contents) {
+		await mkdir(dirname(path), { recursive: true });
+		await writeFile(path, contents);
+	}
+};
+
+const processCss = async (css, inPath, outPath, maps) => {
+	const result = await postcss([
+	    autoprefixer(),
+	    cssnano()
+	]).process(css, { from: inPath, to: outPath, map: maps ? {inline: false} : false });
+	// do not copy empty css files
+	if(result.css.indexOf("\n/*# source") !== 0){
+		await makeFile(outPath, result.css);
 	}
 }
 
-const processCss = (css, inPath, outPath, maps) => {
-	postcss([
-	    autoprefixer(),
-	    cssnano()
-	])
-	.process(css, { from: inPath, to: outPath, map: maps ? {inline: false} : false })
-	.then(result => {
-		if(result.css.indexOf("\n/*# source") === 0){
-			makeFileSync(outPath, result.css);
-		}
-	})
-}
-
-const resultToMapAndCss = (err, result, inPath, outPath, maps) => {
+const resultToMapAndCss = async (err, result, inPath, outPath, maps) => {
 	if (err) throw err;
 	if(maps && result.map && JSON.parse(result.map).mappings){
-		makeFileSync(outPath + ".map", result.map);
+		await makeFile(outPath + ".map", result.map);
 	} 
-    processCss(result.css, inPath, outPath, maps);
+    await processCss(result.css, inPath, outPath, maps);
 }
 
-module.exports = (inPath, outPath, options) => {
+export default async (inPath, outPath, options) => {
 	const maps = options && options.maps;
+	const contents = await readFile(inPath, 'utf-8');
 	if(type(inPath) === 'less'){
-		less.render(fs.readFileSync(inPath, 'utf8'), { sourceMap: {} }, (err, result) => {
-		    resultToMapAndCss(err, result, inPath, outPath, maps);
+		renderLess(contents, { sourceMap: {} }, async (err, result) => {
+		    await resultToMapAndCss(err, result, inPath, outPath, maps);
 		})
 	}else if (type(inPath) === 'sass') {
-		sass.render({
+		renderSass({
 		    file: inPath,
 		    outFile: outPath,
 		    sourceMap: true
-		}, (err, result) => {
-			resultToMapAndCss(err, result, inPath, outPath, maps);
+		}, async (err, result) => {
+			await resultToMapAndCss(err, result, inPath, outPath, maps);
 		});
 	}else if (type(inPath) === 'css') {
-		processCss(fs.readFileSync(inPath, 'utf8'), inPath, outPath, false);
+		await processCss(contents, inPath, outPath, false);
 	}
 };
 
--- test.js
+++ test.js
@@ -1,24 +1,31 @@
-var XtoCss = require('.');
+import XtoCss from './index.js';
 
-const now = Date.now();
+console.log(1);
 // scss to css
 XtoCss("test/test.scss", "dist/path/to/dist.css", {
-	maps: true
+	maps: false
 });
-
+console.log(2);
 // css to css
-XtoCss("test/tester.css","dist/path/to/dister.css");
-
+XtoCss("test/tester.css","dist/path/to/dister.css", {
+	maps: false
+});
+console.log(3);
 // scss with no includes to css
-XtoCss("test/no-includes.scss","dist/path/to/no-include.min.css");
-
+XtoCss("test/no-includes.scss","dist/path/to/no-include.min.css", {
+	maps: false
+});
+console.log(4);
 // scss includes with no "content" to css (shouldn't create a file)
-XtoCss("test/nested/inc.scss","dist/path/to/xyz.css");
-
+XtoCss("test/nested/inc.scss","dist/path/to/xyz.css", {
+	maps: false
+});
+console.log(5);
 // less with no includes to css
-XtoCss("test/test.less","dist/path/to/lesstest.css");
-
-console.log((Date.now() - now));
+XtoCss("test/test.less","dist/path/to/lesstest.css", {
+	maps: false
+});
+console.log(6);
 
 
 /*
